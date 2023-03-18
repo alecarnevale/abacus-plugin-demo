14f296f7dd689efa34d60a7de598cac521cbc8a8
alecarnevale <alecarnevale.dev@gmail.com>
2023-03-18 15:34:58 +0100

    configure abacus

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,3 +11,10 @@ plugins {
   id("org.jetbrains.kotlin.jvm") version "1.8.0"
   id("com.alecarnevale.abacus") version "1.0.0"
 }
+
+abacus {
+  val cls = listOf("Continent", "Felidae", "Africa", "Asia", "Europe", "NorthAmerica", "SouthAmerica", "Oceania", "Antarctica", "Pantherinae", "Felinae")
+  val ext = listOf(Pair("xml", "ancient"), Pair("xml", "natural"), Pair("xml", "new"))
+  supertypes.set(cls)
+  fileDescriptors.set(ext)
+}
